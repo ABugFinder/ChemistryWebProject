8c8087af603545c97820dbf584f5110828390802
JanPalaciosC <56079698+JanPalaciosC@users.noreply.github.com>
2020-04-29 17:33:21 -0600

    Update easyMemorama.blade.php

--- resources/views/games/easyMemorama.blade.php
+++ resources/views/games/easyMemorama.blade.php
@@ -2,6 +2,9 @@
 @section('content')
 
     <div id="cont-game">
+    
+         <!--Aqui aparece el contador-->
+        <a id="contenedor" style="text-decoration: none; color: #636b6f;"></a>
         
         <!--Frame of the cards-->
         <section id="card-container" class="memory-game"></section>
